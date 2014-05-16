2896c0da0bfec443692820b1fe5b2f5e95461b9a
Team Simplicit <support@team-simplicity.com>
2014-05-16 22:01:03 +0200

    chacha: remove unneeded defines from cm.mk

--- cm.mk
+++ cm.mk
@@ -15,9 +15,6 @@ PRODUCT_NAME := cm_chacha
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := ChaCha
-PRODUCT_VERSION_DEVICE_SPECIFIC :=
-BUILD_ID := JLS36I
-ROM_BUILDTYPE := HOMEMADE
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_chacha BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_chacha/chacha:4.3.1/$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="2.13.401.2 CL197017 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_chacha BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_chacha/chacha:4.4.2/$(BUILD_ID)/v1.5:user/release-keys PRIVATE_BUILD_DESC="2.13.401.2 CL197017 release-keys"
