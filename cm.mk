1f781e3d30cfeaa61b4540dc2c04a84910f891d6
olivieer <y.karasangabo@bg-rams.ac.at>
2014-07-03 05:24:16 -0400

    chacha: cleanup cm.mk
    * gsm.mk doesn't (actually) exist, see:
    https://github.com/androidarmv6/android_vendor_cm/blob/cm-11.0/config/gsm.mk
    * There's no need for a custom build fingerprint
    
    Change-Id: I69fdc22178b7829c569b8d3e6226d9398677ff06

--- cm.mk
+++ cm.mk
@@ -1,20 +1,20 @@
+# Licensed under the Apache License, Version 2.0.
+# Copyright (C) 2014 The Android Open Source Project
+# Copyright (C) 2014 The CyanogenMod Project
+
 # Call our device's AOSP device config.
 $(call inherit-product, device/htc/chacha/full_chacha.mk)
-
-# Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
-$(call inherit-product, vendor/cm/config/mini.mk)
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 320
 TARGET_SCREEN_WIDTH := 480
 
+# Inherit common CM stuff
+$(call inherit-product, vendor/cm/config/mini.mk)
+
 # Overrides for CM
-PRODUCT_DEVICE := chacha
 PRODUCT_NAME := cm_chacha
+PRODUCT_DEVICE := chacha
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := ChaCha
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_chacha BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_chacha/chacha:4.4.2/$(BUILD_ID)/v1.5:user/release-keys PRIVATE_BUILD_DESC="2.13.401.2 CL197017 release-keys"
